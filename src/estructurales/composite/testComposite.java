5911f33a605d8a432db772fac72262ad2a241980
Nicholas <gianfrancopr08@gmail.com>
2024-06-25 15:45:09 -0500

    Implementación del patrón Composite.

--- src/estructurales/composite/testComposite.java
+++ src/estructurales/composite/testComposite.java
@@ -2,22 +2,18 @@ package estructurales.composite;
 
 public class testComposite {
     public static void main(String[] args) {
-        // Crear objetos simples
-        Componente objeto1 = new Elemento("Libro", 1);
-        Componente objeto2 = new Elemento("Laptop", 2);
-        Componente objeto3 = new Elemento("Teléfono", 3);
+        Componente libro = new Elemento("Libro", 1);
+        Componente laptop = new Elemento("Laptop", 2);
+        Componente vidrio = new Elemento("Vidrio", 3);
 
-        // Crear una caja y agregarle objetos
         Caja caja1 = new Caja("Caja1");
-        caja1.addComponente(objeto1);
-        caja1.addComponente(objeto2);
+        caja1.addComponente(libro);
+        caja1.addComponente(laptop);
 
-        // Crear otra caja y agregarle un objeto y la primera caja
         Caja caja2 = new Caja("Caja2");
-        caja2.addComponente(objeto3);
+        caja2.addComponente(vidrio);
         caja2.addComponente(caja1);
 
-        // Mostrar el contenido y el precio total de caja2
         caja2.mostrar();
         System.out.println("Precio total de caja2: " + caja2.calcularPrecio());
     }
