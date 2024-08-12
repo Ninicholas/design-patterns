85c068a153ea8ce353e06a8eae94af66dfbe22fd
Nicholas <gianfrancopr08@gmail.com>
2024-08-12 15:11:56 -0500

    Implementación del patrón Comando

--- src/comportamiento/Comando/LightOffCommand.java
+++ src/comportamiento/Comando/LightOffCommand.java
@@ -2,7 +2,7 @@ package comportamiento.Comando;
 
 // Comando concreto para apagar las luces
 public class LightOffCommand implements Command {
-    private Light light;
+    private final Light light;
 
     public LightOffCommand(Light light) {
         this.light = light;
