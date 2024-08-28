a3f976ffd42e3c5a8d4b755c97451716ee518196
Ninicholas <gianfrancopr08@gmail.com>
2024-08-28 16:49:58 -0500

    Implementación del patrón Estrategia

---
+++ src/comportamiento/estrategia/CryptoPayment.java
@@ -0,0 +1,14 @@
+package comportamiento.estrategia;
+
+public class CryptoPayment implements PaymentStrategy {
+    private String walletAddress;
+
+    public CryptoPayment(String walletAddress) {
+        this.walletAddress = walletAddress;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paid " + amount + " using Cryptocurrency.");
+    }
+}
